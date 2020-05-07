d42e0b430f0145c3bf3800db9e37f15116a9a25a
Merely_igor_homePC <mogila1708@gmail.com>
2020-05-07 23:08:19 +0300

    добавил

--- script/install_ru.sh
+++ script/install_ru.sh
@@ -181,9 +181,10 @@ function config_delivery() {
 ####################################################################################
   У вас есть ссылка на репо с кастомным конфигом config.plist ?
   Что за такое репо с кастомным конфигом вы могли прочитать в REDME
-  ссылка на него https://github.com/merelyigor/My_custom_config_plist
+  ссылка на него https://github.com/merelyigor/Script-install-EFI-B360M-A_i3-9100F_RX580/blob/master/REDME_RU.md#%D1%87%D1%82%D0%BE-%D0%B7%D0%B0-%D1%82%D0%B0%D0%BA%D0%BE%D0%B5-%D1%80%D0%B5%D0%BF%D0%BE-%D1%81-%D0%BA%D0%B0%D1%81%D1%82%D0%BE%D0%BC%D0%BD%D1%8B%D0%BC-%D0%BA%D0%BE%D0%BD%D1%84%D0%B8%D0%B3%D0%BE%D0%BC-
 
   Учтите что репозиторий должен соответствовать этим пунктам
+  О том как создать свое репо с настроенным config.plist можно по ссылке
   1). Название репозитория должно быть                                    My_custom_config_plist
   2). Файл (config.plist) должен находиться в репо по такому пути         My_custom_config_plist/EFI/CLOVER/config.plist
   3). Если ваш репозиторий приватный у вас должен быть установлен ключь SSH или другой
