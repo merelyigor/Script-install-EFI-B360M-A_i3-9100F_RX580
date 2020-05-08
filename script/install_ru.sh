f679a8bf1624063f6f0aabd7b68990ad9d34044d
Merely_igor_homePC <mogila1708@gmail.com>
2020-05-08 23:44:59 +0300

    Update install_ru.sh

--- script/install_ru.sh
+++ script/install_ru.sh
@@ -202,10 +202,10 @@ function config_check() {
 ####################################################################################
   У вас есть ссылка на репо с кастомным конфигом config.plist ?
   Что за такое репо с кастомным конфигом вы могли прочитать в REDME
-  ссылка на него https://github.com/merelyigor/Script-install-EFI-B360M-A_i3-9100F_RX580/blob/master/REDME_RU.md#%D1%87%D1%82%D0%BE-%D0%B7%D0%B0-%D1%82%D0%B0%D0%BA%D0%BE%D0%B5-%D1%80%D0%B5%D0%BF%D0%BE-%D1%81-%D0%BA%D0%B0%D1%81%D1%82%D0%BE%D0%BC%D0%BD%D1%8B%D0%BC-%D0%BA%D0%BE%D0%BD%D1%84%D0%B8%D0%B3%D0%BE%D0%BC-
+  ссылка на REDME https://git.io/JfCek
 
-  О том как создать свое репо с настроенным config.plist можно по ссылке ниже
-  https://github.com/merelyigor/Script-install-EFI-B360M-A_i3-9100F_RX580/blob/master/REDME_RU.md#%D1%82%D0%B0%D0%BA-%D0%BA%D0%B0%D0%BA-%D0%B6%D0%B5-%D1%81%D0%B4%D0%B5%D0%BB%D0%B0%D1%82%D1%8C-%D1%82%D0%B0%D0%BA%D0%BE%D0%B5-%D1%81%D0%B2%D0%BE%D0%B5-%D1%80%D0%B5%D0%BF%D0%BE-%D1%87%D1%82%D0%BE%D0%B1-%D0%B2%D1%81%D0%B5-%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%B0%D0%BB%D0%BE-%D0%BF%D0%BE-%D0%BC%D0%B0%D1%81%D0%BB%D1%83-
+  О том как создать свое репо с настроенным config.plist можно по ссылке https://git.io/JfCet
+
 
  Учтите что репозиторий должен соответствовать этим пунктам
   (). Название репозитория должно быть                                    My_custom_config_plist
