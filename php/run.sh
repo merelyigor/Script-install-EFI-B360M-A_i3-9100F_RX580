1e1105c354f000cf744bd4bd3e97d1aa6183c327
Merely_igor_homePC <mogila1708@gmail.com>
2020-05-09 22:12:56 +0300

    Update run.sh

--- php/run.sh
+++ php/run.sh
@@ -3,7 +3,7 @@
 #======================== Run scripts PHP =======2.0
 #---------------------------------------------------
 
-cd ~/GitHub/MY\ HACKINTOS\ EFI-FILES/Script-install-EFI-B360M-A_i3-9100F_RX580/php || exit 
+cd ~/GitHub/MY\ HACKINTOS\ EFI-FILES/Script-install-EFI-B360M-A_i3-9100F_RX580/php || exit
 
 chmod +x console.php
 
